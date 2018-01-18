efee0d07391b07966aaa0f372b3b646c9887716d
ilgulee <iglee2000ca@gmail.com>
2018-01-18 09:53:33 -0500

    SqlConnection and Simple Queries

--- DBConnectionLegacy/Program.cs
+++ DBConnectionLegacy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,42 @@ namespace DBConnectionLegacy
     {
         static void Main(string[] args)
         {
+            var connection=new SqlConnection("Data Source=LEE;Initial Catalog=StudentTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = null;
+                var command=new SqlCommand("SELECT * FROM STUDENTS",connection);
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1}", 
+                            reader["FirstName"].ToString(), 
+                            reader["LastName"].ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data available");
+                }
+                
+                reader.Close();
+                
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            Console.ReadKey();
         }
     }
 }
